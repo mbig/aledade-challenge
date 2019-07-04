27cea4f505d09b727dda099b6b080f641c70563e
Gefter Chongong <gefter.chongong@civicactions.com>
2019-07-04 09:05:07 -0400

    changes to resources

--- resources.tf
+++ resources.tf
@@ -10,65 +10,36 @@ resource "aws_key_pair" "prod-keypair" {
 }
 
 
-
 # postgres instance  inside dev private subnet
 resource "aws_instance" "dev-postgresdb-instance" {
    ami  = "${var.ami}"
    instance_type = "t2.micro"
    key_name = "${aws_key_pair.dev-keypair.id}"
-   subnet_id = "${aws_subnet.dev-private-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.dev-sgdb.id}"]
-   source_dest_check = false
-
-  tags {
-    Name = "dev postgresql instance"
-  }
-}
-
-# Dev bastion host
-resource "aws_instance" "dev-bastion" {
-   ami  = "${var.ami}"
-   instance_type = "t2.micro"
-   key_name = "${aws_key_pair.dev-keypair.id}"
+   #subnet_id = "${aws_subnet.dev-private-subnet.id}"
+   #vpc_security_group_ids = ["${aws_security_group.dev-sgdb.id}"]
    subnet_id = "${aws_subnet.dev-public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.dev-sgweb.id}"]
-   source_dest_check = false
-   associate_public_ip_address = true
+   vpc_security_group_ids = ["${aws_security_group.dev-sg.id}"]
+   #source_dest_check = false
 
   tags {
-    Name = "dev bastion instance"
+    Name = "dev-postgresql-instance"
   }
 }
 
 
-# postgres instance  inside prod private subnet
+
+
+# Prod db instance
 resource "aws_instance" "prod-postgresdb-instance" {
    ami  = "${var.ami}"
    instance_type = "t2.micro"
    key_name = "${aws_key_pair.prod-keypair.id}"
-   subnet_id = "${aws_subnet.prod-private-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.prod-sgdb.id}"]
-   source_dest_check = false
-
-
-  tags {
-    Name = "prod postgresql instance"
-  }
-}
-
-
-# prod bastion host
-resource "aws_instance" "prod-bastion" {
-   ami  = "${var.ami}"
-   instance_type = "t2.micro"
-   key_name = "${aws_key_pair.prod-keypair.id}"
    subnet_id = "${aws_subnet.prod-public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.prod-sgweb.id}"]
-   source_dest_check = false
-   associate_public_ip_address = true
+   vpc_security_group_ids = ["${aws_security_group.prod-sg.id}"]
+
 
 
   tags {
-    Name = "prod bastion instance"
+    Name = "prod-postgresql-instance"
   }
 }
