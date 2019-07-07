3f88abf23dd45071a566be5500f581b039030bf0
Gefter Chongong <mbijeff@gmail.com>
2019-07-07 13:37:51 -0400

    fixing parenthenses

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ sudo pip install boto
 echo "Installing terraform 0.11.8"
 wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 sudo unzip terraform_0.11.8_linux_amd64.zip -d /usr/local/sbin
-echo Iinitializing terraform"
+echo "Iinitializing terraform"
 terraform init
