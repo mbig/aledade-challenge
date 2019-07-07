9a3ea19864c43edc39348470af10956816f56c46
Gefter Chongong <mbijeff@gmail.com>
2019-07-07 13:48:43 -0400

    ssh-keygen creation

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ echo "Installing python2-pip"
 sudo yum install python2-pip -y
 echo "Installing boto"
 sudo pip install boto
+echo "creating ssh keys"
+ssh-keygen
 echo "Installing terraform 0.11.8"
 wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 sudo unzip terraform_0.11.8_linux_amd64.zip -d /usr/local/sbin
